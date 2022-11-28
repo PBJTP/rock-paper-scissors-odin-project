0074ca6965fe89d9ff50933a7230e32563d5feac
PBJTP <jplypick1990@gmail.com>
2022-11-27 23:33:01 -0800

    Broke stuff, fixed stuff, more tomorrow

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function getComputerChoice() {
+let playerScore = 0;
+let computerScore = 0;
+
+/* function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3) + 1;
     if (choice === 1) {
         return "rock";
@@ -15,40 +18,80 @@ function getPlayerChoice() {
     let choice = prompt("Choose Rock, Paper or Scissors");
     choice = choice.toLowerCase();
     return choice;
-    /* if (choice !== 'rock' || choice === 'paper' || choice === 'scissors') {
+    if (choice !== 'rock' || choice === 'paper' || choice === 'scissors') {
         confirm("Sorry, not a valid choice. Please type Rock, Paper, or Scissors! (not case sensitive)");
     } else {
         return choice;
-    } */
-};
+    } 
+}; */
 
-let playerSelection = getPlayerChoice(); 
+function game() {
 
-console.log(playerSelection);
+    let computerSelection = getComputerChoice();
+    let playerSelection = prompt("Choose Rock, Paper or Scissors").toLowerCase();
+    /*function getPlayerChoice() {
+        let choice = prompt("Choose Rock, Paper or Scissors");
+        choice = choice.toLowerCase();
+        return choice;
+        /* if (choice !== 'rock' || choice === 'paper' || choice === 'scissors') {
+            confirm("Sorry, not a valid choice. Please type Rock, Paper, or Scissors! (not case sensitive)");
+        } else {
+            return choice;
+        } 
+    }; */
 
-let computerSelection = getComputerChoice();
-console.log(computerSelection); 
+    function getComputerChoice() {
+        let choice = Math.floor(Math.random() * 3) + 1;
+        if (choice === 1) {
+            return "rock";
+        } else if (choice === 2) {
+            return "paper";
+        } else if (choice === 3) {
+            return "scissors";
+        } else {
+            return "error";
+        }
+    };
 
-
-function playRound (getPlayerChoice, getComputerChoice) {
-
-    if (playerSelection === computerSelection) {
-        return "It's a tie!";
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You Lose, the computer chose paper";
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You Win! The computer chose Scissors";
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You Win! The computer chose Rock";
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You Lose, the computer chose Scissors";
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You Win! the computer chose Paper";
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You Lose, the computer chose Rock";
+    function playRound(playerSelection, computerSelection) {
+        
+        if (playerSelection === computerSelection) {
+            return "It's a tie!";
+        } else if (playerSelection === "rock" && computerSelection === "paper") {
+            computerScore ++;
+            return "You Lose, the computer chose paper";
+        } else if (playerSelection === "rock" && computerSelection === "scissors") {
+            playerScore ++;
+            return "You Win! The computer chose Scissors";
+        } else if (playerSelection === "paper" && computerSelection === "rock") {
+            playerScore ++;
+            return "You Win! The computer chose Rock";
+        } else if (playerSelection === "paper" && computerSelection === "scissors") {
+            computerScore ++;
+            return "You Lose, the computer chose Scissors";
+        } else if (playerSelection === "scissors" && computerSelection === "paper") {
+            playerScore ++;
+            return "You Win! the computer chose Paper";
+        } else if (playerSelection === "scissors" && computerSelection === "rock") {
+            computerScore ++;
+            return "You Lose, the computer chose Rock";
+        }
     }
-} 
 
-let result = playRound();
+if (computerScore == 5) {
+    return "YOU LOSE THE BATTLE";
+} else if (playerScore == 5) {
+    return "YOU WIN THE BATTLE"
+} else {
+    playRound(playerSelection, computerSelection);
+}
 
-console.log(result);
+console.log(computerSelection);
+console.log("your score is " + playerScore);
+console.log("Computer score is " + computerScore);
+
+}
+
+
+
+game();
