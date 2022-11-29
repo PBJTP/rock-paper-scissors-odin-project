61ea8fe495540e49943994a5b613a33518433e4c
PBJTP <jplypick1990@gmail.com>
2022-11-29 00:03:44 -0800

    Finish project. Add helper functions when we revisit

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let compScore = 0;
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3) + 1;
     if (choice === 1) {
@@ -15,11 +18,6 @@ function getPlayerChoice() {
     let choice = prompt("Choose Rock, Paper or Scissors");
     choice = choice.toLowerCase();
     return choice;
-    if (choice !== 'rock' || choice === 'paper' || choice === 'scissors') {
-        confirm("Sorry, not a valid choice. Please type Rock, Paper, or Scissors! (not case sensitive)");
-    } else {
-        return choice;
-    } 
 };
 
 function playRound(playerSelection, computerSelection) {
@@ -30,24 +28,39 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "It's a tie!";
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        //computerScore ++;
+        compScore ++;
         return "You Lose, the computer chose paper";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        //playerScore ++;
+        playerScore ++;
         return "You Win! The computer chose Scissors";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        //playerScore ++;
+        playerScore ++;
         return "You Win! The computer chose Rock";
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        //computerScore ++;
+        compScore ++;
         return "You Lose, the computer chose Scissors";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        //playerScore ++;
+        playerScore ++;
         return "You Win! the computer chose Paper";
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        //computerScore ++;
+        compScore ++;
         return "You Lose, the computer chose Rock";
     }
-}
+};
 
-console.log(playRound(getPlayerChoice(), getComputerChoice()));
+function game() {
+
+    while (playerScore < 5 && compScore < 5) {
+        console.log(playRound(getPlayerChoice(), getComputerChoice()));
+        console.log("Your score is " + playerScore);
+        console.log("The computer's score is " + compScore);
+
+        if (compScore === 5) {
+            return console.log("YOU LOSE THE BATTLE!!!");
+        } else if (playerScore === 5) {
+            return console.log("YOU WIN THE BATTLE!!!");
+        }
+    }
+};
+
+game();
